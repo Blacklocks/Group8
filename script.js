ebda68036249abf3c55470a1cfbfd64abd9c5534
Blacklocks <woody_851@hotmail.com>
2020-10-18 10:28:23 +1100

    added retrieve movie call to both isbn searches

--- script.js
+++ script.js
@@ -62,6 +62,9 @@ function retrieveISBN(){
         "<h2>" + response.docs[i].title + 
         "<h2>" + response.docs[i].author_name[0] +
         "<br><img src = 'http://covers.openlibrary.org/b/isbn/" + response.docs[i].isbn[0] + "-M.jpg'><br>";
+        bookTitle =response.docs[i].title;
+        console.log(bookTitle); // to test book title has been read
+        retrieveMovie(bookTitle);
       }
     }
   );
@@ -97,7 +100,7 @@ function retrieveISBNbutton(x){
         "<h2>" + response.docs[i].author_name[0] +
         "<br><img src = 'http://covers.openlibrary.org/b/isbn/" + response.docs[i].isbn[0] + "-M.jpg'><br>";
         bookTitle =response.docs[i].title;
-        console.log(bookTitle); 
+        console.log(bookTitle); // to test book title has been read
         retrieveMovie(bookTitle);
       }
     }
